72ae3b1f9a857787dac8d368a7beb30853e26e93
Hungdzpro2506 <134621730+Hungdzpro2506@users.noreply.github.com>
2024-11-19 21:14:27 +0700

    Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            
+        const double rate = 23000.0;
+
+       
+        Console.Write("Nhập giá trị USD: ");
+        double usdValue = double.Parse(Console.ReadLine());
+
+        
+        double vndValue = usdValue * rate;
+        Console.WriteLine($"{usdValue} USD = {vndValue} VND");
         }
     }
 }
